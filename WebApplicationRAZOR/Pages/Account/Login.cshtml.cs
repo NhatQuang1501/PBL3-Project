41053acad29be98a9f8c2fdfcd87aa2dc754ff26
NhatQuang1501 <nhatquang1512003@gmail.com>
2023-05-11 18:37:09 +0700

    Thử Log in

---
+++ WebApplicationRAZOR/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace PBL3_Project.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+            this.Credential = new Credential { UserName = "admin" };
+        }
+        public void OnPost()
+        {
+        }
+    }
+
+    public class Credential
+    {
+        [RequiredAttribute]
+        [Display(Name="Tên đăng nhập")]
+        public string UserName { get; set; }
+        [RequiredAttribute]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mật khẩu")]
+        public string Password { get; set; }
+    }
+}
