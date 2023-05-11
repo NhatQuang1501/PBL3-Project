ccb6aa545733cdd9e671e4b6eeeb8c5533f9d3a9
NhatQuang1501 <nhatquang1512003@gmail.com>
2023-05-11 12:02:45 +0700

    Thêm Home page và Info page

---
+++ WebApplicationRAZOR/Pages/Parent/PInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationRAZOR.Pages.Parent
+{
+    public class PInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebApplicationRAZOR/Pages/Tutor/THome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationRAZOR.Pages.Tutor
+{
+    public class THomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
