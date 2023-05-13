c1c682c27e99ab4d4e389e4bc241c5543b004270
Buianhvu226 <102667078+Buianhvu226@users.noreply.github.com>
2023-05-13 21:11:10 +0700

    9h10-5/13/2023

---
+++ WebApplicationRAZOR/Pages/Tutor/TPersonal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PBL3_Project.Pages.Tutor
+{
+    public class TPersonalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
