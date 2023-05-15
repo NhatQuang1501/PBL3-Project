faf65afcf3fe76ec1ba34c4665aacdf6b304296e
NhatQuang1501 <nhatquang1512003@gmail.com>
2023-05-15 23:39:20 +0700

    Đổi tên page đúng, thay link Trang Index thành About, trang Đăng nhập thành Login trong Account

--- WebApplicationRAZOR/Pages/Index.cshtml.cs
+++ WebApplicationRAZOR/Pages/Index.cshtml.cs
@@ -1,20 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApplicationRAZOR.Pages
+namespace PBL3_Project.Pages
 {
-    public class IndexModel : PageModel
+    public class AboutModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
         public void OnGet()
         {
-
         }
     }
 }
