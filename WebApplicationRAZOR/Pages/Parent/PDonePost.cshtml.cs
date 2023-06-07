fdd6905213762e3949e94947963f652d21181084
hongnhung225 <tranthihongnhung2205@gmail.com>
2023-06-07 18:04:21 +0700

    18h03-7/6/2023

--- WebApplicationRAZOR/Pages/Parent/PDonePost.cshtml.cs
+++ WebApplicationRAZOR/Pages/Parent/PDonePost.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PBL3_Project.Data;
+using PBL3_Project.Models;
 
 namespace PBL3_Project.Pages.Parent
 {
     public class PDonePostModel : PageModel
     {
+        private readonly PBL3_ProjectContext _context;
+        public List<BaiDang> Posts { get; set; }
+        public PDonePostModel(PBL3_ProjectContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+            Posts = _context.BaiDang.Where(p => p.TinhTrangDuyet == true && p.TinhTrang == true).ToList();
         }
     }
 }
--- WebApplicationRAZOR/Pages/Parent/PHome.cshtml.cs
+++ WebApplicationRAZOR/Pages/Parent/PHome.cshtml.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PBL3_Project.Data;
 using PBL3_Project.Models;
 
 namespace WebApplicationRAZOR.Pages.Parent
 {
     public class PHomeModel : PageModel
     {
+        private readonly PBL3_ProjectContext _context;
+        public List<BaiDang> Posts { get; set; }
+        public PHomeModel(PBL3_ProjectContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
-
+            //Posts = _context.BaiDang.ToList();
+            Posts = _context.BaiDang.Where(p => p.TinhTrangDuyet == true && p.TinhTrang == false).ToList();
         }
 
         
