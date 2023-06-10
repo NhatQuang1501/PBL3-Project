30327b16a9669aa0a436d21c83595fd258f3b70c
ch1ckenD4nce <hththam22@gmail.com>
2023-06-10 17:13:40 +0700

    tdonepost, tdangkipost

--- WebApplicationRAZOR/Pages/Tutor/TDonePost.cshtml.cs
+++ WebApplicationRAZOR/Pages/Tutor/TDonePost.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PBL3_Project.Data;
+using PBL3_Project.Models;
 using System.Data;
 
 namespace PBL3_Project.Pages.Tutor
@@ -8,8 +10,31 @@ namespace PBL3_Project.Pages.Tutor
     //[Authorize(Roles = $"{RolesApp.Tutor}")]
     public class TDonePostModel : PageModel
     {
+        //public void OnGet()
+        //{
+        //}
+        private readonly PBL3_ProjectContext _context;
+        public List<SuatDayDaNhan> SuatDayDaNhans { get; set; }
+        public BaiDang BaiDang { get; set; }
+        public List<BaiDang> Posts { get; set; }
+
+       // public SuatDayDaNhan SuatDayDaNhan { get; set; }
+
+        public TDonePostModel(PBL3_ProjectContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+            //Posts = _context.BaiDang.ToList();
+            //SuatDayDaNhans = _context.SuatDayDaNhan.Where(p => p.ID_GiaSu == 5).ToList();
+            //foreach (var SuatDayDaNhan in SuatDayDaNhans)
+            //{
+            //    Posts = _context.BaiDang.Where(p => p.ID_BaiDang == SuatDayDaNhan.ID_BaiDang).ToList();
+            //}
+            var idBaiDangList = _context.SuatDayDaNhan.Where(sddk => sddk.ID_GiaSu == 5).Select(sddk => sddk.ID_BaiDang).ToList();
+            Posts = _context.BaiDang.Where(bd => idBaiDangList.Contains(bd.ID_BaiDang)).ToList();
         }
+
     }
 }
