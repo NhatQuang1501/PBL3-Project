0aa803b9cc670a15d6dd2a9546c764f38c2cd5c3
Buianhvu226 <102667078+Buianhvu226@users.noreply.github.com>
2023-06-11 13:50:14 +0700

    13h49-11-06-2023

--- WebApplicationRAZOR/Pages/Tutor/THome.cshtml.cs
+++ WebApplicationRAZOR/Pages/Tutor/THome.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using PBL3_Project.Data;
 using PBL3_Project.Models;
+using PBL3_Project.ViewModel;
 
 namespace WebApplicationRAZOR.Pages.Tutor
 {
@@ -11,6 +12,8 @@ namespace WebApplicationRAZOR.Pages.Tutor
     {
         private readonly PBL3_ProjectContext _context;
         public List<BaiDang> Posts { get; set; }
+        [BindProperty]
+        public Filter Model { get; set; }
         public THomeModel(PBL3_ProjectContext context)
         {
             _context = context;
@@ -19,6 +22,71 @@ namespace WebApplicationRAZOR.Pages.Tutor
         {
             //Posts = _context.BaiDang.ToList();
             Posts = _context.BaiDang.Where(p => p.TinhTrangDuyet == true && p.TinhTrang == false).ToList();
+        }
+        public IActionResult OnPostByFilter()
+        {
+            var query = _context.BaiDang.Where(p => p.TinhTrangDuyet == true && p.TinhTrang == false).AsQueryable();
+
+
+            string monhoc = Request.Form["MonHoc"];
+            monhoc = monhoc.Replace(" ", "").ToLower();
+
+            string lop = Request.Form["Lop"];
+
+            string khuvuc = Request.Form["KhuVuc"];
+            khuvuc = khuvuc.Replace(" ", "").ToLower();
+
+            string sohv = Request.Form["SoHocVien"];
+
+            string sobuoi = Request.Form["SoBuoi"];
+
+            string hocphimax = Request.Form["HocPhiMax"];
+            string hocphimin = Request.Form["HocPhiMin"];
+
+            if (!string.IsNullOrEmpty(monhoc))
+            {
+                query = query.Where(p => p.MonHoc.Replace(" ", "").ToLower() == monhoc.Replace(" ", "").ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(lop))
+            {
+                query = query.Where(p => p.Lop.Replace(" ", "").ToLower() == lop.Replace(" ", "").ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(khuvuc))
+            {
+                query = query.Where(p => p.DiaChi.Replace(" ", "").ToLower().Contains(khuvuc.Replace(" ", "").ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(sohv))
+            {
+                query = query.Where(p => p.SoHocVien.ToString() == sohv);
+            }
+
+            if (!string.IsNullOrEmpty(sobuoi))
+            {
+                query = query.Where(p => p.SoBuoi.ToString() == sobuoi);
+            }
+
+            if (!string.IsNullOrEmpty(hocphimax))
+            {
+                query = query.Where(p => Compare(p.HocPhi.ToString(), hocphimax.ToString()) <= 0);
+            }
+
+            if (!string.IsNullOrEmpty(hocphimin))
+            {
+                query = query.Where(p => Compare(p.HocPhi.ToString(), hocphimin.ToString()) >= 0);
+            }
+
+
+
+            Posts = query.ToList();
+            return Page();
+        }
+
+        private int Compare(string v1, string v2)
+        {
+            return string.Compare(v1, v2);
         }
     }
 }
